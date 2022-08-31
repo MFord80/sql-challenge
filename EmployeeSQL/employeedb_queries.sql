78e4e8d9b51bb4ec332fcdb3b0e680001883245b
MFord80 <matthewfford@gmail.com>
2022-08-31 19:51:35 +0800

    tables sql, erd updated

---
+++ EmployeeSQL/employeedb_queries.sql
@@ -0,0 +1,2 @@
+-- employeedb queries
+
---
+++ EmployeeSQL/employeedb_tables.sql
@@ -0,0 +1,56 @@
+﻿-- Table schemata for employeesdb
+
+-- Create tables including primary and foreign keys
+
+
+CREATE TABLE "titles" (
+    "title_id" varchar(5)   NOT NULL,
+    "title" varchar(30)   NOT NULL UNIQUE,
+    PRIMARY KEY (title_id)
+);
+
+
+CREATE TABLE "employees" (
+    "emp_no" integer   NOT NULL,
+    "emp_title_id" varchar(5)   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar(50)   NOT NULL,
+    "last_name" varchar(50)   NOT NULL,
+    "sex" varchar(1)   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT pk_employees PRIMARY KEY (emp_no),
+		FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+
+CREATE TABLE "salaries" (
+    "emp_no" integer   NOT NULL,
+    "salary" integer   NOT NULL,
+    CONSTRAINT pk_salaries PRIMARY KEY (emp_no),
+		FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+
+CREATE TABLE "departments" (
+    "dept_no" varchar(4)   NOT NULL,
+    "dept_name" varchar(40)   NOT NULL UNIQUE,
+    CONSTRAINT pk_departments PRIMARY KEY (dept_no)
+);
+
+
+CREATE TABLE "dept_emp" (
+    "emp_no" integer   NOT NULL,
+    "dept_no" varchar(4)   NOT NULL,
+    CONSTRAINT pk_dept_emp PRIMARY KEY (emp_no,dept_no),
+		FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+		FOREIGN KEY (dept_no) REFERENCES departments(dept_no)	
+);
+
+
+CREATE TABLE "dept_manager" (
+    "dept_no" varchar(4)   NOT NULL,
+    "emp_no" integer   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (emp_no,dept_no),
+		FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+		FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
